8928b5d917a6f1894d96e786fedec46471b84be5
devneelam <neelam.nageena@gmail.com>
2025-05-16 22:18:17 +0100

    check for file and folder path in alpine transmission script

--- install/alpine-transmission-install.sh
+++ install/alpine-transmission-install.sh
@@ -17,6 +17,26 @@ msg_info "Installing Transmission"
 $STD apk add --no-cache transmission-cli transmission-daemon
 $STD rc-service transmission-daemon start
 $STD rc-service transmission-daemon stop
+
+FOLDER_PATH="/var/lib/transmission/config"
+mkdir -p $FOLDER_PATH
+
+FILE_PATH=$FOLDER_PATH"/settings.json"
+
+if [ -d $FOLDER_PATH  ]; then
+  echo "Folder exists at $FOLDER_PATH."    
+else
+  echo "Folder does not exist"
+fi
+
+if [ $test -f $filename  ]; then
+  echo "File exists at $FILE_PATH."
+else
+  echo "File does not exist"
+fi
+
+exit 1
+
 sed -i '{s/"rpc-whitelist-enabled": true/"rpc-whitelist-enabled": false/g; s/"rpc-host-whitelist-enabled": true,/"rpc-host-whitelist-enabled": false,/g}' /var/lib/transmission/config/settings.json
 msg_ok "Installed Transmission"
 
